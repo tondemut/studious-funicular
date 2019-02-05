f6dd137ba26d4e69ef46863b87e5baf2574ac7b0
Tonderai Mutanda <tonderaimutanda@gmail.com>
2019-02-05 14:25:22 +0200

    Added Testimonial Section

--- js/main.js
+++ js/main.js
@@ -142,3 +142,12 @@
 
 
 }());
+
+// When the DOM is ready, run this function
+$(document).ready(function() {
+  //Set the carousel options
+  $('#quote-carousel').carousel({
+    pause: true,
+    interval: 4000,
+  });
+});
